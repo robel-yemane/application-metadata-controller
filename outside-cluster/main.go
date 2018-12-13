a4104a3c5429d176bb1e9627f31878efab4769ab
Robel Yemane <ryemane@statestreet.com>
2018-12-13 17:37:07 +0000

    pod/deployment etc listing added

--- outside-cluster/main.go
+++ outside-cluster/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -15,6 +16,37 @@ func main() {
 	}
 	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-	// clientset.AppsV1().DaemonSets
+	// list all deployments in the default namespace
+	defaultNamespace := apiv1.NamespaceDefault
+	deploymentClient := clientset.AppsV1().Deployments(defaultNamespace)
+	fmt.Printf("\n=> Listing deployments in namespace %q:\n\n", defaultNamespace)
+	list, err := deploymentClient.List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, d := range list.Items {
+		fmt.Printf(" * %s (%d replicas)\n", d.Name, *d.Spec.Replicas)
+	}
 
+	// list all namespaces
+	nsClient := clientset.CoreV1().Namespaces()
+	fmt.Printf("\n=> Listing all namespaces: \n\n")
+	ns, err := nsClient.List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, n := range ns.Items {
+		fmt.Printf(" * %s\n", n.Name)
+	}
+
+	// list pods in default namespace
+	fmt.Printf("\n=>Listing Pods in namespace %q:\n\n", defaultNamespace)
+	podClient := clientset.CoreV1().Pods(defaultNamespace)
+	podList, err := podClient.List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, pod := range podList.Items {
+		fmt.Printf(" * %s\n", pod.Name)
+	}
 }
