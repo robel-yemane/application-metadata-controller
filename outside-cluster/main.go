ac786a4bcfc0cfcf15c84a458dd18aaa1f9110cc
Robel Yemane <ryemane@statestreet.com>
2018-12-20 17:35:42 +0000

    some more stuff

--- outside-cluster/main.go
+++ outside-cluster/main.go
@@ -50,14 +50,41 @@ func main() {
 		fmt.Printf(" * %s\n", pod.Name)
 	}
 
-	fmt.Println("\n=>Listing Pods in namespace kube-system:\n")
+	fmt.Println("\n=>Listing Pods in namespace kube-system - with status running:\n")
 
 	podClientII := clientset.CoreV1().Pods("kube-system")
-	podListII, err := podClientII.List(metav1.ListOptions{})
+	listOptions := metav1.ListOptions{
+		FieldSelector: "status.phase=Running",
+	}
+	podListII, err := podClientII.List(listOptions)
 	if err != nil {
 		panic(err)
 	}
+
 	for _, pod := range podListII.Items {
+
 		fmt.Printf(" * %s\n", pod.Name)
 	}
+
+	cmClient := clientset.CoreV1().ConfigMaps(apiv1.NamespaceAll)
+
+	fmt.Printf("\n=> Listing all CMs and their NS: \n\n")
+	cm, err := cmClient.List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, c := range cm.Items {
+		fmt.Printf(" * %s	-> %s\n", c.Namespace, c.Name)
+	}
+	//------//
+	// list a namespace
+	ks, err := clientset.CoreV1().Services("kube-system").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, something := range ks.Items {
+		fmt.Printf(" %s -> %s\n", something.GetObjectMeta().GetName(), something.GetObjectMeta().GetSelfLink())
+	}
+
 }
